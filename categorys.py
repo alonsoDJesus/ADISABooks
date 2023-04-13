781501aad16b32db8ca7c821dac0717fae45c79b
Diana Laura <dianalauraequihuadiaz@gmail.com>
2023-04-13 02:49:08 -0600

    Se agrego el modelo de la categorias

---
+++ categorys.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from odoo import models,fields
+
+class categorys(models.Model):
+    _name = 'library.categorys'
+
+    name= fields.Char(string='Lista de Categorias',required=True)
+    books_id = fields.One2many('library.books','id_categoria',string='Libros',readonly=True)
+    
+    _order = 'name'
+    _sql_constraints = [('categorys_uniq', 'unique(name)', 'Categoria duplicada, intenta con otra...'),]

3901dba95d9091bbd682753ffc743d42b15d6a10
Diana Laura <dianalauraequihuadiaz@gmail.com>
2023-04-13 02:44:59 -0600

    Se agregaron los campos de los identificadores de Categorias e Editoriales

--- books.py
+++ books.py
@@ -5,18 +5,12 @@ from odoo import models,fields
 class books(models.Model):
     _name = 'library.books'
 
-    isbn= fields.Char(string='ISBN')
-    name= fields.Char(string='Titulo')
-    anio_edicion= fields.Char(string='Año de edicion')
-    no_paginas= fields.Integer(string='Número de Páginas')
-    existencias= fields.Integer(string='Número de Existencia')
+    isbn= fields.Char(string='ISBN', required=True)
+    name= fields.Char(string='Titulo', required=True)
+    anio_edicion= fields.Char(string='Año de edicion',required=True)
+    no_paginas= fields.Integer(string='Número de Páginas', required=True)
+    existencias= fields.Integer(string='Número de Existencia', required=True)
+    id_categoria= fields.Many2one('library.categorys',string='Categoria', required=True)
+    id_editorial= fields.Many2one('library.editorials',string='Editorial', required=True)
     
     _order = 'name,isbn'
-    
-class category(models.Model):
-    _name = 'library.category'
-
-    id_categoria= fields.Many2one('library.books',string='Identificador de Categoria')
-    name= fields.Integer(string='Nombre')
-    
-    _order = 'id_categoria'

673a5d2a9163ba74525e6a3d34d101b9764c06ab
Alonso de Jesus <alonsodjesus.vargas.rincon@hotmail.com>
2023-04-14 16:07:49 -0600

    eliminación de comentarios

--- authors.py
+++ authors.py
@@ -6,16 +6,7 @@ class authors(models.Model):
     a_paterno = fields.Char( string='Apellido Paterno', required=True)
     name = fields.Char( string='Nombre', required=True )
     degree = fields.Char( string= 'Grado Académico' )
-    ##book_id = fields.Many2one( 'library.books', string = 'Libro' )
-    ##book_ids = fields.One2many( 'library.books', 'author_id', string = 'Libros' )
     book2_ids = fields.Many2many('library.books',string='Libros')
-    #id_author_book = fields.Many2One('library.authors_books', string = )
 
     _order = 'name'
     _sql_constraints = [ ('lastName_authors_uniq', 'unique(a_paterno, name)', 'Autor duplicado, escribe uno diferente.') ]
-#class authors_books(models.Model):
- #   _name = 'library.authors_books'
-  #  books_id = fields.One2Many( 'library.books', 'id_author_book', string = 'Libros', readonly=True )
-   # authors_id = fields.One2Many( 'library.authors', 'id_author_book', string = 'Autores', readonly=True )
-    
-

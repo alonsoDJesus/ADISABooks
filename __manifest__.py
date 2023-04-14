b37348551dbe9616983d2f636003f21e9446ba8f
Alonso de Jesus <alonsodjesus.vargas.rincon@hotmail.com>
2023-04-14 11:59:54 -0600

    correcciones

--- __manifest__.py
+++ __manifest__.py
@@ -12,16 +12,10 @@
         'views/books_view.xml',
         'views/categorys_view.xml',
         'views/editorials_view.xml',
-<<<<<<< HEAD
-=======
         'views/authors_view.xml',
         'views/students_view.xml',
         'views/careers_view.xml',
-<<<<<<< HEAD
         'views/teachers_view.xml',
-=======
->>>>>>> cc81f62f2835c123e485b9b67e66c7f9bfdc695c
->>>>>>> 5d80568dd59e1dd6a1a70531ddf9bc90f56b5660
         'views/menu_view.xml',    
     ],
     'installable': True,

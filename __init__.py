5d80568dd59e1dd6a1a70531ddf9bc90f56b5660
Salma  Almazan Barajas <al19020078@itsa.edu.mx>
2023-04-14 02:40:19 -0600

    No se repiten los isbn y hay varias categorias para cada libro

--- __init__.py
+++ __init__.py
@@ -3,9 +3,5 @@ from . import books
 from . import categorys
 from . import editorials
 from . import authors
-<<<<<<< HEAD
-=======
 from . import students
->>>>>>> cc81f62f2835c123e485b9b67e66c7f9bfdc695c
-
 
--- books.py
+++ books.py
@@ -5,17 +5,13 @@ from odoo import models,fields
 class books(models.Model):
     _name = 'library.books'
 
-<<<<<<< HEAD
     isbn= fields.Char(string='ISBN')
     author=fields.Char(string='Autor')
-=======
     isbn= fields.Char(string='ISBN', required=True)
->>>>>>> cc81f62f2835c123e485b9b67e66c7f9bfdc695c
     name= fields.Char(string='Titulo', required=True)
     anio_edicion= fields.Char(string='Año de edicion',required=True)
     no_paginas= fields.Integer(string='Número de Páginas', required=True)
     existencias= fields.Integer(string='Número de Existencia', required=True)
-<<<<<<< HEAD
     id_categoria= fields.Many2many('library.categorys',string='Categoria')
     id_editorial= fields.Many2one('library.editorials',string='Editorial', required=True)
     author_id = fields.Many2many( 'library.authors', 'book_id', string ='Autor' )
@@ -23,7 +19,6 @@ class books(models.Model):
 
     _order = 'name'
     _sql_constraints = [('isbn_uniq', 'unique(isbn)', 'ISBN duplicada, intenta con otra...'),]
-=======
     id_categoria= fields.Many2one('library.categorys',string='Categoria', required=True)
     id_editorial= fields.Many2one('library.editorials',string='Editorial', required=True)
     author_ids = fields.One2many( 'library.authors', 'book_id', string = 'Autor' )
@@ -31,4 +26,3 @@ class books(models.Model):
     #id_author_book = fields.Many2One('library.authors_books', string='Autor', required=True)
     
     _order = 'name,isbn'
->>>>>>> cc81f62f2835c123e485b9b67e66c7f9bfdc695c

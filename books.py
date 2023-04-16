9e8620efd173b63a4660c2d0b74ec4e5bd505fc2
Salma  Almazan Barajas <al19020078@itsa.edu.mx>
2023-04-15 20:11:03 -0600

    Vista categorias

--- books.py
+++ books.py
@@ -5,16 +5,15 @@ from odoo import models,fields
 class books(models.Model):
     _name = 'library.books'
 
-    isbn= fields.Char(string='ISBN', required=True)
+    isbn= fields.Char(string='ISBN')
     name= fields.Char(string='Titulo', required=True)
     anio_edicion= fields.Char(string='Año de edicion',required=True)
     no_paginas= fields.Integer(string='Número de Páginas', required=True)
     existencias= fields.Integer(string='Número de Existencia', required=True)
-    id_categoria= fields.Many2one('library.categorys',string='Categoria', required=True)
+    id_categoria= fields.Many2many('library.categorys',string='Categoria', required=True)
     id_editorial= fields.Many2one('library.editorials',string='Editorial', required=True)
     author2_ids = fields.Many2many('library.authors',string='Autor(es)')
-    ##author_ids = fields.One2many( 'library.authors', 'book_id', string = 'Autor' )
-    ##author_id = fields.Many2one( 'library.authors', string = 'Autores' )
-    #id_author_book = fields.Many2One('library.authors_books', string='Autor', required=True)
-    
+
+    _sql_constraints = [('isbn_uniq', 'unique(isbn)', 'ISBN duplicada, intenta con otra...'),]
+   
     _order = 'name,isbn'
--- categorys.py
+++ categorys.py
@@ -6,6 +6,6 @@ class categorys(models.Model):
 
     name= fields.Char(string='Lista de Categorias',required=True)
     books_id = fields.One2many('library.books','id_categoria',string='Libros',readonly=True)
-    
+        
     _order = 'name'
     _sql_constraints = [('categorys_uniq', 'unique(name)', 'Categoria duplicada, intenta con otra...'),]
